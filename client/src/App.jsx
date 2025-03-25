1201c3eecee6f1493cd0233d6eb172e4fd45f350
Katherine Abela <katherineabela@mail.weber.edu>
2025-03-24 18:31:18 -0600

    Training Programs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,33 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
-import UserManagement from "./pages/UserManagement"; 
-import CreateTraining from "./pages/CreateTraining"; 
+import UserManagement from "./pages/UserManagement";
+import CreateTraining from "./pages/CreateTraining";
+import TrainingPrograms from "./pages/Trainingprograms";
 
 function App() {
-	const router = createBrowserRouter([
-		{
-			path: "/",
-			element: <Layout />,
-			children: [
-				{
-					index: true,
-					element: <Dashboard />,
-				},
-				{ path: "user-management", element: <UserManagement /> }, 
-				{ path: "create-training", element: <CreateTraining /> }, 
-				// Example of adding a new route
-				// {
-				//   path: 'login',
-				//   element: <Login />
-				// }
-			],
-		},
-	]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
+        { path: "user-management", element: <UserManagement /> },
+        { path: "create-training", element: <CreateTraining /> },
+        { path: "training-programs", element: <TrainingPrograms /> },
+        // Example of adding a new route
+        // {
+        //   path: 'login',
+        //   element: <Login />
+        // }
+      ],
+    },
+  ]);
 
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
