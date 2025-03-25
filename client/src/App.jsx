d85e3680243b0a188a69d9cd0b9c62474db47e45
ztb42 <ztb42@hotmail.com>
2025-03-24 18:46:59 -0600

    Fixed merge conflict in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,30 +4,31 @@ import Dashboard from "./pages/Dashboard";
 import UserManagement from "./pages/UserManagement";
 import CreateTraining from "./pages/CreateTraining";
 import TrainingPrograms from "./pages/Trainingprograms";
+import ErrorPage from "./pages/Error";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          index: true,
-          element: <Dashboard />,
-        },
-        { path: "user-management", element: <UserManagement /> },
-        { path: "create-training", element: <CreateTraining /> },
-        { path: "training-programs", element: <TrainingPrograms /> },
-        // Example of adding a new route
-        // {
-        //   path: 'login',
-        //   element: <Login />
-        // }
-      ],
-    },
-  ]);
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Layout />,
+			children: [
+				{
+					index: true,
+					element: <Dashboard />,
+				},
+				{ path: "user-management", element: <UserManagement /> },
+				{ path: "create-training", element: <CreateTraining /> },
+				{ path: "training-programs", element: <TrainingPrograms /> },
+				// Example of adding a new route
+				// {
+				//   path: 'login',
+				//   element: <Login />
+				// }
+			],
+		},
+	]);
 
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
