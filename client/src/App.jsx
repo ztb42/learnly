53f891e4fa78bb1b11650b447e69ef900c2d68a2
iiblacksmithii <94884566+iiiblacksmithiii@users.noreply.github.com>
2025-03-25 23:01:53 -0600

    Update Navbar before rebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import UserManagement from "./pages/UserManagement";
 import CreateTraining from "./pages/CreateTraining";
 import TrainingPrograms from "./pages/TrainingPrograms";
 import ErrorPage from "./pages/Error";
+import ForgotPassword from "./pages/ForgotPassword";
+import CreateUser from "./pages/CreateUser";
+import "./styles/main.scss";
 
 function App() {
 	const router = createBrowserRouter([
@@ -20,6 +23,9 @@ function App() {
 				{ path: "user-management", element: <UserManagement /> },
 				{ path: "create-training", element: <CreateTraining /> },
 				{ path: "training-programs", element: <TrainingPrograms /> },
+				{ path: "login", element: <Login /> },
+        		{ path: "forgot-password", element: <ForgotPassword /> },
+        		{ path: "create-user", element: <CreateUser /> },
 				// Example of adding a new route
 				// {
 				//   path: 'login',
@@ -28,36 +34,6 @@ function App() {
 			],
 		},
 	]);
-import UserManagement from "./pages/UserManagement"; 
-import CreateTraining from "./pages/CreateTraining"; 
-import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
-import CreateUser from "./pages/CreateUser";
-import "./styles/main.scss";
-
-function App() {
-	const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          index: true,
-          element: <Dashboard />,
-        },
-        { path: "user-management", element: <UserManagement /> },
-        { path: "create-training", element: <CreateTraining /> },
-        { path: "login", element: <Login /> },
-        { path: "forgot-password", element: <ForgotPassword /> },
-        { path: "create-user", element: <CreateUser /> },
-        // Example of adding a new route
-        // {
-        //   path: 'login',
-        //   element: <Login />
-        // }
-      ],
-    },
-  ]);
 
 	return <RouterProvider router={router} />;
 }
