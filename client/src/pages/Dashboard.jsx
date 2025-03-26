094c1fb94cad0a04332965f18f8ddf7f7a1d9bf7
iiblacksmithii <94884566+iiiblacksmithiii@users.noreply.github.com>
2025-03-26 00:22:14 -0600

    dashboard changed back

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -16,14 +16,10 @@ import UserCard from "../components/dashboard/UserCard";
 import Progress from "../components/Progress";
 
 const Dashboard = () => {
-//   const { data: courses, loading: coursesLoading } = useApi(
-//     "/api/training-programs"
-//   );
-//   const { data: users, loading: usersLoading } = useApi("/api/users");
-const { data: courses = [], loading: coursesLoading } = useApi(
-  "/api/training-programs"
-);
-const { data: users = [], loading: usersLoading } = useApi("/api/users");
+  const { data: courses, loading: coursesLoading } = useApi(
+    "/api/training-programs"
+  );
+  const { data: users, loading: usersLoading } = useApi("/api/users");
 
   const [showAllCourses, setShowAllCourses] = useState(false);
   const [showAllUsers, setShowAllUsers] = useState(false);
